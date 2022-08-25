9cc85643a466ca084d4a27a2ee12b8df32b2e7a2
Aonama <k.aonama0320@gmail.com>
2022-08-26 00:33:08 +0900

    implement login function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 pub mod get_req;
+pub mod login;
 pub mod post_req;
 
 use actix_web::{web, App, HttpServer};
@@ -11,6 +12,7 @@ async fn main() -> std::io::Result<()> {
             .service(post_req::echo)
             .service(get_req::get_message)
             .service(post_req::send_messsage)
+            .service(login::login)
             .route("/hey", web::get().to(get_req::manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
