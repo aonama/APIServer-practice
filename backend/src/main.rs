fafed16c7a8fa5b01d9b33f1fc8a9f8e6b24a026
Aonama <k.aonama0320@gmail.com>
2022-08-30 22:36:16 +0900

    configure CORS

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,16 @@
 pub mod get_req;
 pub mod post_req;
 
-use actix_web::{web, App, HttpServer, HttpResponse, Error, error};
+use actix_cors::Cors;
+use actix_web::http::header;
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer};
 use tera::Tera;
 
 async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
-    let mut ctx = tera::Context::new();
-    let view = 
-        tmpl.render("index.html", &ctx)
-            .map_err(|e| error::ErrorInternalServerError(e))?;
+    let ctx = tera::Context::new();
+    let view = tmpl
+        .render("index.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
 
     Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
@@ -16,9 +18,18 @@ async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        // とりあえずすべてのオリジンからのリクエストを許可
+        let cors = Cors::default()
+            .allowed_origin_fn(|_origin, _req_head| true)
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .supports_credentials()
+            .max_age(3600);
         let templates = Tera::new("templates/**/*").unwrap();
 
         App::new()
+            .wrap(cors)
             .service(get_req::hello)
             .service(post_req::echo)
             .service(get_req::get_message)
