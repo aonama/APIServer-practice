d4cd7ff2b404955fd23f31805f9be96067ff58d9
Aonama <k.aonama0320@gmail.com>
2022-08-30 22:19:33 +0900

    add html-templates

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,31 @@
 pub mod get_req;
 pub mod post_req;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, HttpResponse, Error, error};
+use tera::Tera;
+
+async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    let view = 
+        tmpl.render("index.html", &ctx)
+            .map_err(|e| error::ErrorInternalServerError(e))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let templates = Tera::new("templates/**/*").unwrap();
+
         App::new()
             .service(get_req::hello)
             .service(post_req::echo)
             .service(get_req::get_message)
             .service(post_req::send_messsage)
             .route("/hey", web::get().to(get_req::manual_hello))
+            .data(templates)
+            .service(web::resource("/home").to(index))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
